2cca82b8812ad4a4bd91d4830220435c62295609
Mugi Khan <mugikhan@gmail.com>
2024-03-20 10:09:03 +0200

    Clean up exports

--- adapters/adapters.ts
+++ adapters/adapters.ts
@@ -1,5 +1,3 @@
-import { ExpoNextSqliteAdapter } from './expo-next-sqlite-adapter';
-import { ExpoSqliteAdapter } from './expo-sqlite-adapter';
-import { OPSqliteAdapter } from './op-sqlite-adapter';
-
-export { OPSqliteAdapter, ExpoSqliteAdapter, ExpoNextSqliteAdapter };
+export { ExpoNextSqliteAdapter } from './expo-next-sqlite-adapter';
+export { ExpoSqliteAdapter } from './expo-sqlite-adapter';
+export { OPSqliteAdapter } from './op-sqlite-adapter';
